4f2e6b19d9f6a6b56036e2cd0e7304b917c8ab95
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-10-17 02:15:25 +1000

    console

--- models/base_model.py
+++ models/base_model.py
@@ -1,55 +1,87 @@
 #!/usr/bin/python3
-import uuid
-from datetime import datetime
-format_datetime = "%Y-%m-%dT%H:%M:%S.%f"
+"""
+Module containing the BaseModel class.
+"""
 
-"""Define BaseModel class"""
+from datetime import datetime
+from uuid import uuid4
+import models
 
 
 class BaseModel:
-    """Class declaration for baseModel of the clone"""
+    """
+    BaseModel class: defines all common attributes
+    /methods for other classes
+
+    Attributes:
+        id (str): unique identifier for each instance.
+
+        created_at (datetime): datetime object representing
+        the instance creation time.
+
+        updated_at (datetime): datetime object representing the
+        instance last update time.
+
+    Methods:
+        __init__(self, *args, **kwargs): initializes a new BaseModel instance.
+        __str__(self): returns a string representation of
+        the BaseModel instance.
+
+        save(self): updates the public instance attribute
+        `updated_at` with the current datetime.
+
+        to_dict(self): returns a dictionary
+        representation of the BaseModel instance.
+    """
     def __init__(self, *args, **kwargs):
-        """Initializes a new instant.
+        """
+        Initializes a new BaseModel instance.
 
         Args:
-        args: unused
-        kwargs: key/value pair of arguments
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
         """
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__['created_at'] = datetime.strptime(
-                     kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__['updated_at'] = datetime.strptime(
-                     kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
         else:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
-            storage.new(self)
-
-    def to_dict(self):
-        """Dictionary containing all keys/values
-        of __dict__ of the instance"""
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
-
-    def save(self):
-        """Updates the public instance attribute
-        updated_at with the current datetime"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
-        """String representation"""
+        """
+        Returns a string representation of the BaseModel instance.
+
+        Returns:
+            str: String representation of the BaseModel instance.
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
-    
+
+    def save(self):
+        """
+        Updates the public instance attribute
+        updated_at` with the current datetime.
+
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """
+        Returns a dictionary representation of the BaseModel instance.
+
+        Returns:
+            dict: Dictionary containing the attributes of BaseModel.
+        """
+        dicts = self.__dict__.copy()
+        dicts["__class__"] = self.__class__.__name__
+        dicts["created_at"] = self.created_at.isoformat()
+        dicts["updated_at"] = self.updated_at.isoformat()
+        return dicts
