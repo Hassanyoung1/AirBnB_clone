e92decc2ec4e4c0e967b9a07f730c046b7680109
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-10-17 01:34:13 +1000

    Console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,6 +17,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class FileStorage:
     """
     Serializes instances to a JSON file and deserializes
