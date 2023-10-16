2ba373ec968250ef13476f64569c60418e3a7943
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-10-17 00:30:10 +1000

    updated

--- console.py
+++ console.py
@@ -39,7 +39,7 @@ class HBNBCommand(cmd.Cmd):
     """
 
     intro = "A command interpreter to manipulate AirBnB data"
-    prompt = '(hbnb) '
+    prompt = '(hbnb)'
 
     objects = {
         'BaseModel': BaseModel, 'User': User, 'State': State,
@@ -47,11 +47,12 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def do_quit(self, args):
-        """Quit command to exit the program"""
+        """Quit command to exit the program """
+        
         return True
 
     def do_EOF(self, args):
-        """Quit command to exit the program"""
+        """Exit the program"""
         return True
 
     def emptyline(self):
